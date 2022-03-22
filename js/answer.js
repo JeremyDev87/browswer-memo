0453c36a95fd030eddf31059f7e4024968d04fae
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-22 23:40:28 +0900

    Last commit

--- js/answer.js
+++ js/answer.js
@@ -1,3 +1,4 @@
+//Question
 // 바탕화면(쪽지가 아닌 회색부분)에 마우스 우클릭시 마우스 위치가 top, left값을 가지는 새로운 쪽지를 생성합니다. answer.html 참고(기본 크기 div.textarea : width:200px, height:100px;)
 // 쪽지의 헤더 부분 드래그시 쪽지를 바탕화면 내에서 이동이 가능해야합니다.(Drag & Drop 플러그인 사용금지, 직접구현해야 함)
 // 드래그 드랍 또는 내용 수정시에 해당하는 쪽지는 겹쳐진 쪽지 중 최상단으로 나와야합니다.
@@ -5,16 +6,25 @@
 // 쪽지 우 하단 드래그시 크기가 변경되어야 합니다. 크기 변경은 div.textarea의 width, height가 변경되어야 합니다.
 // 모든 쪽지 내용, 위치, 크기, 쌓이는 순서는 localStorage에 저장되어야 하며, 리로드시 모든 쪽지가 그대로 나와야합니다.
 
-
 //To-do 
 //1. 우클릭 시 신규 쪽지 생성(기본 크기 div.textarea : width:200px, height:100px;)
-//2. 헤더 드래그 시 이동, 플러그인 금지
-//3. 드래그 앤드랍 시 겹쳐진 쪽지 중 최상단으로 노출
-//4. X 클릭 시 삭제
+//2. 헤더 드래그 시 이동, 드래그 사용 금지
+//3. 포커스온 이나 마우스다운 시 쪽지 최상단 노출
+//4. X 클릭 시 쪽지 삭제 
 //5. 쪽지 우 하단 드래그 시 크기 변경
 //6. 쪽지 내용, 위치, 크기, 순서 localStorage 저장, 리로드시 쪽지 노출
 
-//0. Memo Object
+//Readme 
+//메모의 재사용성을 고려하여 class 활용하였습니다.
+//메모 객체에 대한 기능(생성, 삭제, 수정, 저장)은 prototype으로 활용하였습니다. 
+//메모 순서는 별도로 저장하지 않고, localStorage의 배열 index를 순서로 사용합니다.
+//update prototype에서 새로 localstorage로 delete , insert과정을 통해 새로 마지막 index에 저장하므로
+//생성, 수정 순으로 브라우저에 제공할 수 있습니다. 
+//다만 mousedown, focus의 경우, update가 돌지 않으므로 z-index로 처리하였습니다. 
+//focus만 있을 경우 수정이 없으므로 새로고침 시 최상단 처리하지 않았습니다.
+//저장된 메모가 없을 시 1개는 기본으로 생성하게 했습니다.
+
+//Memo Object
 class Memo {
     constructor(id, position, size, contents) {
         this.position = position;
@@ -73,6 +83,10 @@ class Memo {
 let memoList = [];
 //Init Setting 
 setArrInit();
+//Create default when have not Memo 
+if (!memoList.length > 0) {
+    createMemo(null, { top: 100, left: 100 }, { width: 200, height: 100 }, "");
+}
 setHTML();
 existMemoEvents();
 //Get Data from localStorage
@@ -106,7 +120,7 @@ function existMemoEvents() {
 
 //DOM event for Create New Memo 
 document.addEventListener("contextmenu", e => {
-    createMemo(null, { top: 100, left: 300 }, { width: 200, height: 100 }, "메세지를 입력하세요");
+    createMemo(null, { top: 100, left: 100 }, { width: 200, height: 100 }, "");
     e.preventDefault();
 })
 //Create dependency Event 
@@ -117,8 +131,7 @@ function createEvent(obj, event) {
             obj.remove()
             setArrInit();
         })
-    }
-    if (event === "changeSize") {
+    } else if (event === "changeSize") {
         let sizeInfo = {
             id: 0,
             x: 0,
@@ -126,6 +139,7 @@ function createEvent(obj, event) {
             width: 0,
             height: 0,
         }
+        let memoDiv = document.querySelector(`.wrap > #memo_${obj.id}`);
         let sizeBtn = document.querySelector(`.wrap > #memo_${obj.id}>.content>.btn_size`);
         let changeSizeTarget = document.querySelector(`.wrap > #memo_${obj.id}>.content>.textarea`);
         sizeBtn?.addEventListener('mousedown', (e) => {
@@ -140,12 +154,14 @@ function createEvent(obj, event) {
             document.addEventListener('mouseup', mouseUpHandler);
         })
         const mouseMoveHandler = (e) => {
+            memoDiv.style.opacity = 0.7;
             let dx = e.clientX - sizeInfo.x;
             let dy = e.clientY - sizeInfo.y;
             changeSizeTarget.style.width = `${sizeInfo.width + dx}px`;
             changeSizeTarget.style.height = `${sizeInfo.height + dy}px`;
         }
         const mouseUpHandler = function () {
+            memoDiv.style.opacity = 1;
             let updateObj = {
                 width: Number(changeSizeTarget.style.width.replace('px', '')),
                 height: Number(changeSizeTarget.style.height.replace('px', ''))
@@ -154,8 +170,7 @@ function createEvent(obj, event) {
             document.removeEventListener('mousemove', mouseMoveHandler);
             document.removeEventListener('mouseup', mouseUpHandler);
         };
-    }
-    if (event === "move") {
+    } else if (event === "move") {
         let positionInfo = {
             id: 0,
             x: 0,
@@ -177,12 +192,14 @@ function createEvent(obj, event) {
             document.addEventListener('mouseup', mouseUpHandler);
         })
         const mouseMoveHandler = (e) => {
+            memoDiv.style.opacity = 0.7;
             let dx = e.clientX - positionInfo.x;
             let dy = e.clientY - positionInfo.y;
             memoDiv.style.left = `${positionInfo.left + dx}px`;
             memoDiv.style.top = `${positionInfo.top + dy}px`;
         }
         const mouseUpHandler = function () {
+            memoDiv.style.opacity = 1;
             let updateObj = {
                 top: Number(memoDiv.style.top.replace('px', '')),
                 left: Number(memoDiv.style.left.replace('px', ''))
@@ -191,14 +208,12 @@ function createEvent(obj, event) {
             document.removeEventListener('mousemove', mouseMoveHandler);
             document.removeEventListener('mouseup', mouseUpHandler);
         };
-    }
-    if (event === "write") {
+    } else if (event === "write") {
         let textDiv = document.querySelector(`.wrap > #memo_${obj.id}>.content>.textarea`);
         textDiv.addEventListener('input', () => {
             updateMemo(obj.id, "contents", textDiv.innerHTML)
         })
-    }
-    if (event === "focus") {
+    } else if (event === "focus") {
         let textDiv = document.querySelector(`.wrap > #memo_${obj.id}>.content>.textarea`);
         textDiv.addEventListener('focus', () => {
             indexingMemo(obj.id);
